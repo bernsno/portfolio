39a929a7b477626558f0d9ef05da95c5ca91fb89
Noah Bernsohn <bernsno@gmail.com>
2009-12-11 00:11:40 -0600

    removes unnecessary login test

--- test/integration/admin/works_controller_test.rb
+++ test/integration/admin/works_controller_test.rb
@@ -1,13 +1,7 @@
 require File.dirname(__FILE__) + '/../../test_helper'
 
 class Admin::WorksControllerTest < ActionController::IntegrationTest
-  context "a non-admin visiting admin section" do
-    setup { visit new_admin_work_path }
-    should "return to new user session path" do
-      assert_equal login_url, current_url
-    end
-  end
-  
+
   context "as a user" do
     setup { login_as(Factory(:user)) }
     context "adding a new work" do
